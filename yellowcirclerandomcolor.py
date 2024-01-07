9dfd2c05ab289aa8b38b2b2889de696d90dcbbb5
Alexeyokey <146419947+Alexeyokey@users.noreply.github.com>
2024-01-07 18:21:58 +0300

    random color circle

--- yellowcirclerandomcolor.py
+++ yellowcirclerandomcolor.py
@@ -1,27 +1,29 @@
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5 import uic, QtGui
-from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel
+from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel, QPushButton, QWidget
 import random
 import sys
+
 
 class Circle(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.label = QLabel()
+        self.widget = QWidget()
+        self.label = QLabel(self.widget)
         self.label.setPixmap(QtGui.QPixmap(800, 800))
-        self.setCentralWidget(self.label)
+        self.setCentralWidget(self.widget)
         self.initUI()
 
     def initUI(self):
+        self.pushButton = QPushButton(self.widget)
         self.setFixedSize(800, 800)
-        uic.loadUi('UI.ui', self)
         self.pushButton.clicked.connect(self.draw)
         self.pushButton.move(350, 400)
-        self.color = QColor("yellow")
 
     def draw(self):
         qp = QPainter(self.label.pixmap())
-        qp.setPen(self.color)
+        color = QColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+        qp.setPen(color)
         size = random.randint(1, 200)
         coords = (random.randint(0, 750), random.randint(0, 750))
         qp.drawEllipse(*coords, size, size)
